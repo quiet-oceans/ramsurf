868171e9155ec8e248dfeb2d113d3ffdc26952b9
serge guelton <serge.guelton@telecom-bretagne.eu>
2013-09-03 11:22:16 +0200

    Update header of ramsurf.c
    * Licensing / author informations
    * Use constants instead of defines

--- ramsurf.c
+++ ramsurf.c
@@ -1,10 +1,39 @@
-#define mr 40000
-#define mz 50002
-#define mp 10
-#define m 40
-#define BUFSIZE 1024
+/*
+ * Copyright (c) 2006, Dr. David C. Calvo <calvo@ccs.nrl.navy.mil> Copyright
+ * (c) 2012, Serge Guelton <serge.guelton@quiet-oceans.com>
+ * 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *
+ *     Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *     Neither the name of the <ORGANIZATION> nor the names of its contributors
+ *     may be used to endorse or promote products derived from this software
+ *     without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
 
 #include <stdio.h>
+#include <stddef.h>
 #include <complex.h>
 #include <math.h>
 #include <assert.h>
@@ -28,6 +57,12 @@
 #ifndef min
    #define min( a, b ) ( ((a) < (b)) ? (a) : (b) )
 #endif
+
+static const size_t mr = 40000;
+static const size_t mz = 50002;
+static const size_t mp = 10;
+static const size_t m = 40;
+static const size_t BUFSIZE = 1024;
 
 typedef float complex fcomplex;
 typedef double complex dcomplex;
